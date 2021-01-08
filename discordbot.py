1c6f3ea92197e2b8fb8c4c9105cbb5a4b108e1e9
nitfumic <ringstrap@gmail.com>
2021-01-09 02:41:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,9 +14,11 @@ async def on_message(message):
     if 'こんにちは' in message.content:
         m = 'こんにちは' + message.author.name + 'ヒヒーン'
         await message.channel.send(m)
-        
+
 @client.event
 async def on_member_join(member):
-    message.channel.send(member.display_name)
+    for channel in member.server.channels:
+        if channel.name == 'general' or channel.name == '一般':
+            await client.send_message(channel, member.display_name + 'ちゃん初めましてだヒヒン!')
 
 client.run(token)
