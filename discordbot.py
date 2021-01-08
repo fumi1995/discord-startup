57cd7e9b6fb3952cb26c84ec8baaa7d7a2f25327
nitfumic <ringstrap@gmail.com>
2021-01-09 02:14:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,9 @@
 import discord
-from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('ヒヒーン')
 
 @client.event
 async def on_message(message):
@@ -26,5 +12,4 @@ async def on_message(message):
     if 'こんにちは' in message.content:
         await message.channel.send('こんにちはヒヒーン')
 
-bot.run(token)
 client.run(token)
