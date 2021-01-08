e67442d3fd8f48a3218e9dc1fc5a50e3d40a47e4
nitfumic <ringstrap@gmail.com>
2021-01-09 02:27:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,8 @@ import traceback
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
+
+
 @client.event
 async def on_message(message):
     if message.author.bot:
@@ -12,5 +14,9 @@ async def on_message(message):
     if 'こんにちは' in message.content:
         m = 'こんにちは' + message.author.name + 'ヒヒーン'
         await message.channel.send(m)
+        
+@client.event
+async def on_member_join(member):
+    message.channel.send(member.display_name)
 
 client.run(token)
